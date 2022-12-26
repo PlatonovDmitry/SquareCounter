bc05750287b7058f07236508556aea3b4e2821bb
Дмитрий Платонов <platonov@pointcad.ru>
2022-12-26 08:35:14 +0300

    Запрос в БД

---
+++ GetProductCatsQuery.sql
@@ -0,0 +1,8 @@
+﻿SELECT "Products"."Name", cats.catName 
+FROM "Products"
+LEFT JOIN 
+	(SELECT "ProductCats"."Product" AS product, "Category"."CatName" AS catName
+	FROM "ProductCats"
+	INNER JOIN "Category"
+	ON "ProductCats"."Category" = "Category"."Id")  AS cats
+ON cats.product = "Products"."Id"
